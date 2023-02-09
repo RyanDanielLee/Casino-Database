faaf6775a398fcbe130a2b8bbb99495d198e7bf3
Ryan <ryan.lee8867@outlook.com>
2023-02-08 23:48:03 -0800

    Adds attributes to the Customer entity

---
+++ customers.sql
@@ -0,0 +1,10 @@
+go
+
+CREATE TABLE Customers (
+    Cust_ID int PRIMARY KEY,
+    Cust_LName char(20),
+    Cust_FName char(20),
+    Cust_Age int,
+    Cust_Address char(20),
+    Cust_Phone_num int
+)
